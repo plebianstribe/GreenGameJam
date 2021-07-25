8100ee84117bed933b63d201e2fb32bb9e04bb50
Yew Chong <83965035+yewey2@users.noreply.github.com>
2021-07-25 19:53:34 +0800

    made turtles
    turtle movement
    turtle wobble
    turtle collide into trash

---
+++ Assets/AnimalController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+
+public class AnimalController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public double x_vel;
+    public double y_vel;
+    public double direction;
+    public bool trapped = false; 
+    System.Random random = new System.Random();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        //Random random = new Random();
+        
+        // This is the code for random direction
+        this.x_vel = random.NextDouble();
+        this.y_vel = 1 - this.x_vel;
+        this.x_vel *= 2 * random.Next(0,2) - 1;
+        this.y_vel *= 2 * random.Next(0,2) - 1;
+        this.direction = Math.Atan2(this.x_vel, this.y_vel);
+        rb.rotation =  - Convert.ToSingle(this.direction * (180 / Math.PI));
+        
+        // Setting velocity to test
+        //this.x_vel = 0;
+        //this.y_vel = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!this.trapped) {
+            rb.velocity = new Vector2(Convert.ToSingle(this.x_vel), Convert.ToSingle(this.y_vel));
+        } else {
+            rb.velocity = new Vector2(Convert.ToSingle(random.NextDouble() * 2 - 1), Convert.ToSingle(random.NextDouble() * 2 - 1));
+            //rb.MoveRotation( Convert.ToSingle(random.NextDouble() * (2 * random.Next(0,2) - 1))*10);
+            rb.rotation =  - Convert.ToSingle(this.direction * (180 / Math.PI)) - (Convert.ToSingle(random.NextDouble() * (2 * random.Next(0,2) - 1))*10) ;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.tag == "Trash") {
+            this.trapped = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.tag == "Trash") {
+            this.trapped = false;
+            rb.rotation =  - Convert.ToSingle(this.direction * (180 / Math.PI));
+        }
+    }
+}
+ 
---
+++ Assets/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
