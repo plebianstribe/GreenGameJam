b74abcfd7a159b27aa4843c500348c6ed0b87225
Jagureo <jagnzet@gmail.com>
2021-07-30 23:38:35 +0800

    Added autospawning to AnimalSpawner

--- Assets/AnimalSpawner.cs
+++ Assets/AnimalSpawner.cs
@@ -10,19 +10,29 @@ public class AnimalSpawner : MonoBehaviour
     
     float spawnAreaWidth, spawnAreaHeight;
 
+    public float spawn_timer = 600;
+    float current_timer;
+
     // Start is called before the first frame update
     void Start()
     {
         spawnAreaWidth = spawnArea.GetComponent<SpriteRenderer>().size.x * spawnArea.transform.localScale.x * 0.5f;
         spawnAreaHeight = spawnArea.GetComponent<SpriteRenderer>().size.y * spawnArea.transform.localScale.y * 0.5f;
+
+        current_timer = spawn_timer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (current_timer <= 0)
         {
             spawnAnimal();
+            current_timer = spawn_timer;
+        }
+        else
+        {
+            --current_timer;
         }
     }
 
