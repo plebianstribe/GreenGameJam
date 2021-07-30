35f557967463b2e88f3a07ed167ad6323d8d926f
plebianstribe <nic.holas001@outlook.com>
2021-07-30 20:09:00 +0800

    Merged together UI changes

---
+++ Assets/Mission.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Mission
+{
+    public bool isActive;
+
+    public string missionDescription;
+    public int missionCounter;
+    public int missionTarget;
+}
